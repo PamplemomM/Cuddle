35dc081099d3d3cf8d7f4612eb2143e2593c6110
PamplemomM <tom.marmoz@epitech.eu>
2025-03-09 12:41:31 +0100

    [🚀] - Addition: Basic Header Basic header containing maths macros and defines.

---
+++ include/header_cuddle.h
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2025
+** header_cuddle.h
+** File description:
+** The header for the cuddle project.
+*/
+
+#ifndef CUDDLE_H
+    #define CUDDLE_H
+
+    #include <stddef.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <stdio.h>
+
+    // maths :
+    #define SQ(a) ((a) * (a))
+    #define PI 3.141592653589793238462
+    #define MIN(a, b) ((a) < (b) ? (a) : (b))
+    #define MAX(a, b) ((a) > (b) ? (a) : (b))
+    #define ABS(x) ((x) < 0 ? -(x) : (x))
+
+    //defines :
+    #define SUCCESS 0
+    #define ERROR 84
+
+#endif /* CUDDLE_H */

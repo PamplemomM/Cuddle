c4a5df4bf665917fdff923b2e54ab8a629b6533f
PamplemomM <tom.marmoz@epitech.eu>
2025-03-10 18:18:33 +0100

    [🚀] - Addition: Start of the df_head function This functions will give the n first rows of the dataframe.

--- src/head_tail_csv.c
+++ src/head_tail_csv.c
@@ -6,9 +6,23 @@
 */
 #include "../include/header_cuddle.h"
 
+// **********************************
+// ---- FUNCTION IS NOT FINISHED ----
+// **********************************
+// You can touch it if you want
 dataframe_t *df_head(dataframe_t *dataframe, int nb_rows)
 {
-    return NULL;
+    int rows = 0;
+    dataframe_t *result;
+
+    if (dataframe == NULL || nb_rows <= 0) {
+        return NULL;
+    }
+    if (nb_rows >= dataframe->nb_rows)
+        rows = dataframe->nb_rows;
+    
+    result = malloc(sizeof(dataframe_t) * 1);
+    return result;
 }
 
 dataframe_t *df_tail(dataframe_t *dataframe, int nb_rows)

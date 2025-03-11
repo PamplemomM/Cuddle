306d37e15f1f12b755c62dd5c5a7c823307736dc
PamplemomM <tom.marmoz@epitech.eu>
2025-03-11 13:14:17 +0100

    [🚀] - Addition: Unit tests for detect_type function. Self Explanatory.

---
+++ tests/detect_types_tests.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** detect_types_tests.c
+** File description:
+** test each functions of the detect_types function.
+*/
+
+#include "criterion/criterion.h"
+#include "criterion/redirect.h"
+#include "../include/header_cuddle.h"
+
+//********************************
+// ---- DETECT_TYPES FUNCTION ----
+//********************************
+
+Test(detect_type, full_covr, .init = cr_redirect_stdout)
+{
+    column_type_t type = detect_type("True");
+    mini_printf((type == BOOL) ? "1" : "0");
+    mini_printf("\n");
+    cr_assert_stdout_eq_str("1\n");
+}

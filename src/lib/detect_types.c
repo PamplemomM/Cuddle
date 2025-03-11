10316de3fb60eae6a40e059749d350cbb870dff8
PamplemomM <tom.marmoz@epitech.eu>
2025-03-11 15:09:54 +0100

    [🛠️] - Refactor: Modified the function detect type, Now, it doesn't have to create a new structure on every call, it's just returning the integer.

--- src/lib/detect_types.c
+++ src/lib/detect_types.c
@@ -8,13 +8,13 @@
 
 column_type_t detect_type(char *str)
 {
-    column_type_t result = STRING;
-
+    if (str == NULL)
+        return UNDEFINED;
     if (my_isbool(str) == 1)
-        result = BOOL;
+        return BOOL;
     if (my_isnum(str) == 1)
-        result = (my_getnbr(str) < 0) ? INT : UINT;
+        return (my_getnbr(str) < 0) ? INT : UINT;
     if (my_isfloat(str) == 1)
-        result = FLOAT;
-    return result;
+        return FLOAT;
+    return STRING;
 }

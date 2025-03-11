5cceb5151ade651cf43aa1972bf29aff2f2f3c07
PamplemomM <tom.marmoz@epitech.eu>
2025-03-11 23:43:17 +0100

    [🚀] - Addition: Begining of the function df_describe.

--- src/describe_csv.c
+++ src/describe_csv.c
@@ -8,5 +8,10 @@
 
 void df_describe(dataframe_t *dataframe)
 {
-    return;
+    mini_printf("Column: column name\n");
+    mini_printf("Count: number\n");
+    mini_printf("Mean: mean\n");
+    mini_printf("Std: standart deviation (maths project, lol)\n");
+    mini_printf("Min: min value\n");
+    mini_printf("Max: max value\n");
 }

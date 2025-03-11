d42e7fc852d822b7a13d9098d6634d4fa23c6c3c
TomH <tom.heirich@epitech.eu>
2025-03-11 10:11:19 +0100

    [+] - Added WIP dupbetween function. This function duplicates a given dataframe containing only the data from a given interval of rows.

--- src/head_tail_csv.c
+++ src/head_tail_csv.c
@@ -10,19 +10,64 @@
 // ---- FUNCTION IS NOT FINISHED ----
 // **********************************
 // You can touch it if you want
+// i'm going touch you lelleelelelele
+
+static column_type_t *my_column_types_dup(column_type_t *column_types)
+{
+    int len = 0;
+    column_type_t *res;
+
+    if (column_types == NULL)
+        return NULL;
+    while (column_types[len] != NULL)
+        len++;
+    res = malloc(sizeof(column_type_t) * (len + 1));
+    if (res == NULL)
+        return NULL;
+    for (int i = 0; i < len; i++)
+        res[i] = column_types[i];
+    res[len] = NULL;
+    return res;
+}
+
+static dataframe_t *dupbetween(dataframe_t *dataframe, int start, int end)
+{
+    dataframe_t *result;
+
+    result = malloc(sizeof(dataframe_t));
+    if (result == NULL)
+        return NULL;
+    result->nb_rows = start - end;
+    result->nb_columns = dataframe->nb_columns;
+    result->column_names = my_array_dup(dataframe->column_names);
+    if (result->column_names == NULL) {
+        free(result);
+        return NULL;
+    }
+    result->column_types = my_column_types_dup(dataframe->column_types);
+    if (result->column_types == NULL) {
+        free_word_array(result->column_names);
+        free(result);
+        return NULL;
+    }
+    // result->data = ???;
+    // if (result->data == NULL) {
+    //     free(result->column_types);
+    //     free_word_array(result->column_names);
+    //     free(result);
+    //     return NULL;
+    // }
+    return result;
+}
 
 dataframe_t *df_head(dataframe_t *dataframe, int nb_rows)
 {
-    int rows = 0;
     dataframe_t *result;
 
-    if (dataframe == NULL || nb_rows <= 0) {
+    if (dataframe == NULL || nb_rows <= 0)
         return NULL;
-    }
-    if (nb_rows >= dataframe->nb_rows)
-        rows = dataframe->nb_rows;
-    result = malloc(sizeof(dataframe_t) * 1);
-    return result;
+    nb_rows = MIN(dataframe->nb_rows, nb_rows);
+    return dupbetween(dataframe, 0, nb_rows);
 }
 
 dataframe_t *df_tail(dataframe_t *dataframe, int nb_rows)
@@ -32,9 +77,6 @@ dataframe_t *df_tail(dataframe_t *dataframe, int nb_rows)
     if (dataframe == NULL || nb_rows <= 0)
         return NULL;
     nb_rows = MIN(dataframe->nb_rows, nb_rows);
-    result = malloc(sizeof(dataframe_t));
-    if (result == NULL)
-        return NULL;
-    result->nb_rows = nb_rows;
-    return result;
+    return dupbetween(dataframe,
+    dataframe->nb_rows - nb_rows, dataframe->nb_rows);
 }

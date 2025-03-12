7de4fb7f0036953b6c8a8df5ab2ca7f23b90462b
TomH <tom.heirich@epitech.eu>
2025-03-12 14:56:09 +0100

    [~] - Fixed the 'salade_de_free' unit tests. Self explanatory.

--- tests/lib/my_free_test.c
+++ tests/lib/my_free_test.c
@@ -18,7 +18,7 @@ Test(salade_de_free, full_covr, .init = cr_redirect_stdout)
     char *dup = my_strdup("Tom, really good, mate");
     char **array = my_str_to_word_array(dup, ", ");
 
-    salade_de_free("%as %s", array, dup);
+    salade_de_free("%2 %1", array, dup);
 }
 
 Test(salade_de_free_float, full_covr, .init = cr_redirect_stdout)
@@ -30,7 +30,7 @@ Test(salade_de_free_float, full_covr, .init = cr_redirect_stdout)
     farray[0][1] = 2.5;
     farray[0][2] = 0;
     farray[1] = NULL;
-    salade_de_free("%af", farray);
+    salade_de_free("%2", farray);
 }
 
 Test(salade_de_free_int, full_covr, .init = cr_redirect_stdout)
@@ -42,5 +42,5 @@ Test(salade_de_free_int, full_covr, .init = cr_redirect_stdout)
     iarray[0][1] = 2;
     iarray[0][2] = 0;
     iarray[1] = NULL;
-    salade_de_free("%ad", iarray);
+    salade_de_free("%2", iarray);
 }

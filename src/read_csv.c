379b9a3cd349549ebce3c3e395176e4b819034f3
PamplemomM <tom.marmoz@epitech.eu>
2025-03-12 16:56:57 +0100

    [✅] - Fixed: Read function. Self Explanatory.

--- src/read_csv.c
+++ src/read_csv.c
@@ -94,6 +94,7 @@ int read_csv_next(dataframe_t *data, char *file, char const *separator)
 
     if (lines == NULL)
         return ERROR;
+    data->nb_rows = count_rows(lines[0]) - 1;
     my_print_wa(lines);
     mini_printf("Rows = %d, column = %d", data->nb_rows, data->nb_columns);
     full_data = malloc(sizeof(char **) * (data->nb_rows + 1));
@@ -126,7 +127,7 @@ dataframe_t *df_read_csv(const char *filename, const char *separator)
     else
         sep = my_strdup(separator);
     data->nb_columns = count_columns(file, sep);
-    data->nb_rows = count_rows(file) - 1;
+    
     if (read_csv_next(data, file, sep) == ERROR) {
         free(sep);
         return NULL;

396db5de030c6ac65330b6a4c9c751feb0ce7df9
TomH <tom.heirich@epitech.eu>
2025-03-13 12:57:46 +0100

    [+] - Remade 'df_free' function. The 'df_free' function now frees the dataframe more accurately, and was shortened to 2 lines of code, thanks to the 'salade_de_free' function.

--- src/free_csv.c
+++ src/free_csv.c
@@ -13,7 +13,10 @@
 
 void df_free(dataframe_t *dataframe)
 {
-    if (dataframe == NULL)
+    FREE("%3 %1 %2 %1", dataframe->data, dataframe->column_types,
+        dataframe->column_names, dataframe);
+}
+    /*if (dataframe == NULL)
         return;
     for (int i = 0; i < dataframe->nb_columns; i++) {
         if (dataframe->column_names[i] != NULL)
@@ -23,12 +26,6 @@ void df_free(dataframe_t *dataframe)
         free(dataframe->column_names);
     if (dataframe->column_types != NULL)
         free(dataframe->column_types);
-    if (dataframe->data == NULL) {
-        free(dataframe);
-        return;
-    }
-    for (int i = 0; i < dataframe->nb_columns; i++)
-        FREE("%2", dataframe->data[i]);
-    free(dataframe->data);
-    free(dataframe);
-}
+    if (dataframe->data != NULL)
+        FREE("%2", dataframe->data);
+    free(dataframe);*/

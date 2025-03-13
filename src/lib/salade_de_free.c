ac683cbbc1540146cfa72b6018e81779c41ddb7c
TomH <tom.heirich@epitech.eu>
2025-03-13 12:50:58 +0100

    [+] - Added a NULL argument check to the 'omnifree'. The 'omnifree' function (part of 'salade_de_free') now checks if it's received argument is NULL immediately. This makes it safe to free elements that may have already been freed.

--- src/lib/salade_de_free.c
+++ src/lib/salade_de_free.c
@@ -11,14 +11,14 @@
 
 void *omnifree(void **thing, int dimension)
 {
+    if (thing == NULL)
+        return NULL;
     if (dimension > 1) {
         for (int i = 0; thing[i] != NULL; i++)
             omnifree((void **)thing[i], dimension - 1);
     }
-    if (thing != NULL) {
-        free(thing);
-        thing = NULL;
-    }
+    free(thing);
+    thing = NULL;
     return NULL;
 }
 

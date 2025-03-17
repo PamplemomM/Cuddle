b7e0eb4412cb758584ae1e74b6561a80ea7dc8c4
PamplemomM <tom.marmoz@epitech.eu>
2025-03-17 17:52:55 +0100

    [🚀] - Addition: Beginning of the functions sf_sort. Self Explanatory.

--- src/sort_csv.c
+++ src/sort_csv.c
@@ -6,10 +6,26 @@
 */
 #include "../include/header_cuddle.h"
 
+int sort_dataframe(dataframe_t *dataframe, const char *column,
+    bool(*sort_func)(void *value1, void *value2)
+{
+    return SUCCESS;
+}
+
 dataframe_t *df_sort(dataframe_t *dataframe, const char *column,
     bool(*sort_func)(void *value1, void *value2))
 {
+    dataframe_t *new;
+
     if (dataframe == NULL || sort_func == NULL || column == NULL)
         return NULL;
+    new = malloc(sizeof(dataframe_t));
+    if (new == NULL)
+        return NULL;
+    new->column_names = my_array_dup(dataframe->column_names);
+    if (new->column_names == NULL)
+        return FREE("%1", new);
+    new->nb_columns = dataframe->nb_columns;
+    new->nb_rows = dataframe->nb_rows;
     return dataframe;
 }

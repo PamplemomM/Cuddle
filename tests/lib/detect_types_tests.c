18d232850469e3b3fd1ea6667ce8954ded6c265d
PamplemomM <tom.marmoz@epitech.eu>
2025-03-17 08:30:36 +0100

    [✅] - Fixed: Unit tests.

--- tests/lib/detect_types_tests.c
+++ tests/lib/detect_types_tests.c
@@ -1,8 +1,8 @@
 /*
 ** EPITECH PROJECT, 2023
-** detect_types_tests.c
+** found_data_types_tests.c
 ** File description:
-** test each functions of the detect_types function.
+** test each functions of the found_data_types function.
 */
 
 #include "criterion/criterion.h"
@@ -10,59 +10,8 @@
 #include "../../include/header_cuddle.h"
 
 //********************************
-// ---- DETECT_TYPES FUNCTION ----
+// ---- found_data_typeS FUNCTION ----
 //********************************
-
-Test(detect_type_bool, full_covr, .init = cr_redirect_stdout)
-{
-    column_type_t type = detect_type("True");
-    mini_printf((type == BOOL) ? "1" : "0");
-    mini_printf("\n");
-    cr_assert_stdout_eq_str("1\n");
-}
-
-Test(detect_type_int, full_covr, .init = cr_redirect_stdout)
-{
-    column_type_t type = detect_type("-12");
-    mini_printf((type == INT) ? "1" : "0");
-    mini_printf("\n");
-    cr_assert_stdout_eq_str("1\n");
-}
-
-Test(detect_type_uint, full_covr, .init = cr_redirect_stdout)
-{
-    column_type_t type = detect_type("143");
-    mini_printf((type == UINT) ? "1" : "0");
-    mini_printf("\n");
-    cr_assert_stdout_eq_str("1\n");
-}
-
-Test(detect_type_float, full_covr, .init = cr_redirect_stdout)
-{
-    column_type_t type = detect_type("3.6");
-    mini_printf((type == FLOAT) ? "1" : "0");
-    mini_printf("\n");
-    type = detect_type("-3.6");
-    mini_printf((type == FLOAT) ? "1" : "0");
-    mini_printf("\n");
-    cr_assert_stdout_eq_str("1\n1\n");
-}
-
-Test(detect_type_str, full_covr, .init = cr_redirect_stdout)
-{
-    column_type_t type = detect_type("blablabla");
-    mini_printf((type == STRING) ? "1" : "0");
-    mini_printf("\n");
-    cr_assert_stdout_eq_str("1\n");
-}
-
-Test(detect_type_undefined, full_covr, .init = cr_redirect_stdout)
-{
-    column_type_t type = detect_type(NULL);
-    mini_printf((type == UNDEFINED) ? "1" : "0");
-    mini_printf("\n");
-    cr_assert_stdout_eq_str("1\n");
-}
 
 //*******************************
 // ---- FOUND_TYPES FUNCTION ----
@@ -76,7 +25,6 @@ Test(found_data_type, full_covr, .init = cr_redirect_stdout)
     ptr = found_data_type(NULL, INT);
     ptr = found_data_type("4.86", FLOAT);
     ptr = found_data_type(NULL, FLOAT);
-    ptr = found_data_type(NULL, STRING);
     ptr = found_data_type("true", BOOL);
     ptr = found_data_type(NULL, BOOL);
     ptr = found_data_type("true", UNDEFINED);

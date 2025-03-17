5ef716412534c4a837e4384561a2ad468f99f366
TomH <tom.heirich@epitech.eu>
2025-03-17 11:28:28 +0100

    [~] - Renamed 'my_void_strdup' to 'convert_to_str' in 'src/cuddle_help/found_data_type.c' file, and reordered the functions. The function's name now matches with the rest listed.

--- src/cuddle_help/found_data_type.c
+++ src/cuddle_help/found_data_type.c
@@ -6,26 +6,6 @@
 */
 #include "../../include/header_cuddle.h"
 
-static void *convert_to_int(char const *value)
-{
-    int *result = malloc(sizeof(int));
-
-    if (result == NULL || value == NULL)
-        return NULL;
-    *result = atoi(value);
-    return result;
-}
-
-static void *convert_to_float(char const *value)
-{
-    float *result = malloc(sizeof(float));
-
-    if (result == NULL || value == NULL)
-        return NULL;
-    *result = atof(value);
-    return result;
-}
-
 static void *convert_to_bool(char const *value)
 {
     bool *result = malloc(sizeof(bool));
@@ -33,6 +13,16 @@ static void *convert_to_bool(char const *value)
     if (result == NULL || value == NULL)
         return NULL;
     *result = (my_strcmp(value, "true") == 0 || my_strcmp(value, "1") == 0);
+    return result;
+}
+
+static void *convert_to_int(char const *value)
+{
+    int *result = malloc(sizeof(int));
+
+    if (result == NULL || value == NULL)
+        return NULL;
+    *result = atoi(value);
     return result;
 }
 
@@ -46,7 +36,17 @@ static void *convert_to_uint(char const *value)
     return result;
 }
 
-static void *my_void_strdup(char const *value)
+static void *convert_to_float(char const *value)
+{
+    float *result = malloc(sizeof(float));
+
+    if (result == NULL || value == NULL)
+        return NULL;
+    *result = atof(value);
+    return result;
+}
+
+static void *convert_to_str(char const *value)
 {
     char *result = malloc(sizeof(char) * (my_strlen(value) + 1));
 
@@ -59,7 +59,7 @@ static void *my_void_strdup(char const *value)
 void *found_data_type(char *value, column_type_t type)
 {
     void *(*array[])(char const *) = {convert_to_bool, convert_to_int,
-        convert_to_uint, convert_to_float, my_void_strdup, NULL};
+        convert_to_uint, convert_to_float, convert_to_str, NULL};
 
     if (type >= BOOL && type <= STRING)
         return array[type](value);

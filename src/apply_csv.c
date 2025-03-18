a4a18b9bf93ebc75a8e1252f905aa6532747867d
PamplemomM <tom.marmoz@epitech.eu>
2025-03-18 15:38:37 +0100

    [📜] - Coding-style Fix.

--- src/apply_csv.c
+++ src/apply_csv.c
@@ -10,22 +10,19 @@ dataframe_t *df_apply(dataframe_t *dataframe, const char *column,
     void *(*apply_func)(void *value))
 {
     dataframe_t *result;
-    int value = 0;
+    int val = find_column(dataframe, column);;
     void *tmp = NULL;
 
-    if (dataframe == NULL || column == NULL || apply_func == NULL)
-        return NULL;
-    value = find_column(dataframe, column);
-    if (value == -1)
+    if (val == -1 || dataframe == NULL || column == NULL || apply_func == NULL)
         return NULL;
     result = df_duplicate(dataframe);
     if (result == NULL)
         return NULL;
     for (int i = 0; i < dataframe->nb_rows; i++) {
-        tmp = result->data[i][value];
-        result->data[i][value] = apply_func(tmp);
+        tmp = result->data[i][val];
+        result->data[i][val] = apply_func(tmp);
         free(tmp);
-        if (result->data[i][value] == NULL)
+        if (result->data[i][val] == NULL)
             return FREE("%3 %2 %1 %1", result->data, result->column_names,
                 result->column_types, result);
     }
--- src/cuddle_help/find_column.c
+++ src/cuddle_help/find_column.c
@@ -8,6 +8,8 @@
 
 int find_column(dataframe_t *dataframe, const char *column_name)
 {
+    if (dataframe == NULL || column_name == NULL)
+        return -1;
     for (int i = 0; i < dataframe->nb_columns; i++) {
         if (my_strcmp(dataframe->column_names[i], column_name) == 0)
             return i;

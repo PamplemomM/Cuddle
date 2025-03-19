077962f82bfdce76bdd44c5823cd929dcc54289a
PamplemomM <tom.marmoz@epitech.eu>
2025-03-19 12:23:09 +0100

    [🛠️] - Refactor: df_filter command : Fixed the logic (still need some fixs). Fixed the main logic, but it isn't working.

--- src/filter_csv.c
+++ src/filter_csv.c
@@ -6,72 +6,43 @@
 */
 #include "../include/header_cuddle.h"
 
-static int find_appropriate_column(dataframe_t *result, dataframe_t *dataframe,
-    const char *column_name)
+static int dup_data_condition(dataframe_t *result, dataframe_t *dataframe, int i,
+    int *j)
 {
-    result->nb_rows = dataframe->nb_rows;
-    result->nb_columns = dataframe->nb_columns;
-    for (int i = 0; i < dataframe->nb_columns; i++) {
-        if (my_strcmp(dataframe->column_names[i], column_name) == 0)
-            return i;
+    mini_printf("Writing ");
+    mini_printf("%d in result[%d]\n", *(int *)dataframe->data[i][1], *j);
+    result->data[*j] = df_duprow(dataframe, i);
+    if (result->data[*j] == NULL) {
+        return ERROR;
     }
-    return -1;
-}
-
-int add_row(dataframe_t *result, dataframe_t *data, int row, int column)
-{
-    void **new_row;
-    void ***new_data;
-
-    new_row = malloc(sizeof(void *) * (result->nb_columns + 1));
-    if (new_row == NULL)
-        return ERROR;
-    for (int i = 0; i < result->nb_columns; i++)
-        new_row[i] = data->data[row][i];
-    new_row[result->nb_columns] = NULL;
-    new_data = malloc(sizeof(void **) * (result->nb_rows + 1));
-    if (new_data == NULL)
-        return ERROR;
-    for (int i = 0; i < result->nb_rows; i++)
-        new_data[i] = data->data[i];
-    new_data[row] = new_row;
-    new_data[result->nb_rows] = NULL;
-    result->data = new_data;
-    result->nb_rows++;
+    (*j)++;
+    result->data[*j] = NULL;
     return SUCCESS;
-}
-
-static dataframe_t *dup_types(dataframe_t *dataframe, dataframe_t *result)
-{
-    result->column_types = malloc(sizeof(int *) * (dataframe->nb_columns));
-    if (result->column_types == NULL)
-        return FREE("%2 %3%1", result->column_names, result->data, result);
-    for (int i = 0; i < dataframe->nb_columns; i++) {
-        result->column_types[i] = dataframe->column_types[i];
-    }
-    return result;
 }
 
 dataframe_t *df_filter(dataframe_t *dataframe, const char *column,
     bool(*filter_func)(void *value))
 {
-    dataframe_t *result = malloc(sizeof(dataframe_t));
-    int colomn;
-    int row;
+    dataframe_t *result = NULL;
+    int val = find_column(dataframe, column);
+    int error_check = 0;
+    int j = 0;
 
-    if (result == NULL || dataframe == NULL || column == NULL ||
+    if (val == -1 || dataframe == NULL || column == NULL ||
         filter_func == NULL)
         return NULL;
-    result->column_names = my_array_dup(dataframe->column_names);
-    if (result->column_names == NULL)
-        return FREE("%1", result);
-    colomn = find_appropriate_column(result, dataframe, column);
-    if (colomn == -1)
-        return FREE("%2 %1", result->column_names, result);
-    for (row = 0; row < dataframe->nb_rows; row++) {
-        if (filter_func(dataframe->data[row][colomn]) &&
-            add_row(result, dataframe, row, colomn) == ERROR)
-            return FREE("%2 %3%1", result->column_names, result->data, result);
+    result = df_duplicate(dataframe);
+    if (result == NULL)
+        return NULL;
+    for (int i = 0; i < dataframe->nb_rows; i++)
+        FREE("%2", result->data[i]);
+    for (int i = 0; i < dataframe->nb_rows; i++) {
+        mini_printf("sending to %d from data[%d][%d] and val = %d\n", *(int *)dataframe->data[i][1], i, 1, val);
+        if (filter_func(dataframe->data[i][1]))
+            error_check = dup_data_condition(result, dataframe, i, &j);
+        if (error_check == ERROR)
+            return FREE("%3 %2 %1 %1", result->data, result->column_names,
+                result->column_types, result);
     }
-    return dup_types(dataframe, result);
+    return result;
 }

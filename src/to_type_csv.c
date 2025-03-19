375bdaa866230ca084a600ff452eb39a6b8dac9b
PamplemomM <tom.marmoz@epitech.eu>
2025-03-19 15:57:04 +0100

    [✅] - Fixed: Refixed the df_shape function.

--- src/to_type_csv.c
+++ src/to_type_csv.c
@@ -6,6 +6,32 @@
 */
 #include "../include/header_cuddle.h"
 
+int detect_good_type_inside(dataframe_t *dataframe, int value, int i,
+    column_type_t downcast)
+{
+    if (dataframe->data[i][value] == NULL)
+        return ERROR;
+    if ((downcast == INT || downcast == UINT)
+        && my_isnum(*(char *)dataframe->data[i][value]) == 0)
+        return ERROR;
+    if (downcast == FLOAT && (my_isnum(*(char *)dataframe->data[i][value]) == 0
+        || my_isfloat(*(char *)dataframe->data[i][value]) == 0))
+        return ERROR;
+    if (downcast == BOOL)
+        return SUCCESS;
+    return SUCCESS;
+}
+
+int detect_good_type(dataframe_t *dataframe, int value, column_type_t downcast)
+{
+    column_type_t type = detect_type(dataframe, value, dataframe->nb_rows);
+
+    for (int i = 1; i < dataframe->nb_rows + 1; i++) {
+        detect_good_type_inside(dataframe, value, i, downcast);
+    }
+    return SUCCESS;
+}
+
 dataframe_t *df_to_type(dataframe_t *dataframe, const char *column,
     column_type_t downcast)
 {
@@ -19,7 +45,8 @@ dataframe_t *df_to_type(dataframe_t *dataframe, const char *column,
         return NULL;
     FREE("%3", result->data);
     result->column_types[value] = downcast;
-    if (dup_data(dataframe, result) == ERROR) {
+    if (detect_good_type(dataframe, value, downcast) == ERROR
+        || dup_data(dataframe, result) == ERROR) {
         df_free(result);
         return NULL;
     }

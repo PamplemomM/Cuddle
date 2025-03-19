538610f459acf0c72a0bca8649edb1ba5173b99b
PamplemomM <tom.marmoz@epitech.eu>
2025-03-19 12:36:58 +0100

    [📜] - Coding-style Fix.

--- src/filter_csv.c
+++ src/filter_csv.c
@@ -6,29 +6,27 @@
 */
 #include "../include/header_cuddle.h"
 
-static int dup_data_condition(dataframe_t *result, dataframe_t *dataframe, int i,
-    int *j)
+static int dup_data_condition(dataframe_t *result, dataframe_t *dataframe,
+    int i, int j)
 {
-    mini_printf("Writing ");
-    mini_printf("%d in result[%d]\n", *(int *)dataframe->data[i][1], *j);
-    result->data[*j] = df_duprow(dataframe, i);
-    if (result->data[*j] == NULL) {
-        return ERROR;
+    result->data[j] = df_duprow(dataframe, i);
+    if (result->data[j] == NULL) {
+        FREE("%3 %2 %1 %1", result->data, result->column_names,
+            result->column_types, result);
+        return -1;
     }
-    (*j)++;
-    return SUCCESS;
+    j++;
+    return j;
 }
 
 dataframe_t *df_filter(dataframe_t *dataframe, const char *column,
     bool(*filter_func)(void *value))
 {
     dataframe_t *result = NULL;
-    int val = find_column(dataframe, column);
-    int error_check = 0;
+    int va = find_column(dataframe, column);
     int j = 0;
 
-    if (val == -1 || dataframe == NULL || column == NULL ||
-        filter_func == NULL)
+    if (va == -1 || dataframe == NULL || column == NULL || filter_func == NULL)
         return NULL;
     result = df_duplicate(dataframe);
     if (result == NULL)
@@ -36,12 +34,10 @@ dataframe_t *df_filter(dataframe_t *dataframe, const char *column,
     for (int i = 0; i < dataframe->nb_rows; i++)
         FREE("%2", result->data[i]);
     for (int i = 0; i < dataframe->nb_rows; i++) {
-        mini_printf("sending to %d from data[%d][%d] and val = %d, j = %d\n", *(int *)dataframe->data[i][val], i, val, val, j);
-        if (filter_func(dataframe->data[i][val]))
-            error_check = dup_data_condition(result, dataframe, i, &j);
-        if (error_check == ERROR)
-            return FREE("%3 %2 %1 %1", result->data, result->column_names,
-                result->column_types, result);
+        if (filter_func(dataframe->data[i][va]))
+            j = dup_data_condition(result, dataframe, i, j);
+        if (j == -1)
+            return NULL;
     }
     result->data[j] = NULL;
     result->nb_rows = j;

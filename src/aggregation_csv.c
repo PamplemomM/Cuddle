d74cda6033510e7d1a5c23ee34de709d86b1f760
PamplemomM <tom.marmoz@epitech.eu>
2025-03-21 11:25:20 +0100

    [✅] - Fixed: Fixed the eror handling of the groupby function. Now the function returns null when you give bad arguments.

--- src/aggregation_csv.c
+++ src/aggregation_csv.c
@@ -9,7 +9,8 @@
 dataframe_t *df_groupby(dataframe_t *dataframe, const char *aggregate_by,
     const char **to_aggregate, void *(*agg_func)(void **values, int nb_values))
 {
-    if (dataframe == NULL)
+    if (dataframe == NULL || aggregate_by == NULL || to_aggregate == NULL ||
+        agg_func == NULL)
         return NULL;
     return dataframe;
 }

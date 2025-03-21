f583310d0710e045e06993c3b8d3f1d002491411
TomH <tom.heirich@epitech.eu>
2025-03-21 15:17:19 +0100

    [+] - Added 'df_get_unique_values'. This function iterates through a given column in a dataframe, and returns a list of all unique values from that column.

--- src/get_values_csv.c
+++ src/get_values_csv.c
@@ -37,9 +37,69 @@ void **df_get_values(dataframe_t *dataframe, const char *column)
     return new;
 }
 
+static void **add_value_tolist(void ***list, void *value, column_type_t type)
+{
+    void **new;
+    int len = 0;
+
+    if (*list != NULL) {
+        while ((*list)[len] != NULL)
+            len++;
+    }
+    new = malloc(sizeof(void *) * (len + 2));
+    if (new == NULL)
+        return FREE("%2", *list);
+    for (int i = 0; i < len; i++)
+        new[i] = (*list)[i];
+    new[len] = my_void_dup(value, type);
+    if (new[len] == NULL)
+        return FREE("%1 %2", new, *list);
+    new[len + 1] = NULL;
+    FREE("%1", *list);
+    *list = new;
+    return *list;
+}
+
+static int values_compare(void *v1, void *v2, column_type_t type)
+{
+    if (type == BOOL)
+        return *(bool *)v1 == *(bool *)v2;
+    if (type == INT)
+        return *(int *)v1 == *(int *)v2;
+    if (type == UINT)
+        return *(unsigned int *)v1 == *(unsigned int *)v2;
+    if (type == FLOAT)
+        return *(float *)v1 == *(float *)v2;
+    if (type == STRING)
+        return my_strcmp((char *)v1, (char *)v2) == 0;
+    return 1;
+}
+
+static void **add_if_unique(void ***list, void *value, column_type_t type)
+{
+    if (*list == NULL)
+        return add_value_tolist(list, value, type);
+    for (int i = 0; (*list)[i] != NULL; i++) {
+        if (values_compare((*list)[i], value, type) == 1)
+            return *list;
+    }
+    return add_value_tolist(list, value, type);
+}
+
 void **df_get_unique_values(dataframe_t *dataframe, const char *column)
 {
+    int position = 0;
+    void **new = NULL;
+
     if (dataframe == NULL || column == NULL)
         return NULL;
-    return NULL;
+    position = find_column(dataframe, column);
+    if (position == -1)
+        return FREE("%1", new);
+    for (int i = 0; i < dataframe->nb_rows; i++) {
+        if (add_if_unique(&new, dataframe->data[i][position],
+            dataframe->column_types[position]) == NULL)
+            return NULL;
+    }
+    return new;
 }
